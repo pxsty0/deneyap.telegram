46544baf62d34fc72ed78a0d456463a5e55da39c
Mustafa "pxsty" KÖK <75846829+pxsty0@users.noreply.github.com>
2024-11-17 16:02:38 +0300

    Update DeneyapTelegram.h

--- src/DeneyapTelegram.h
+++ src/DeneyapTelegram.h
@@ -4,26 +4,16 @@
 
 #include <Arduino.h>
 
-#if defined(ESP32)
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
-#else
-#warning "Kütüphane Sadece Deneyap Kartlar İle Çalışmaktadır"
-#include <WiFi.h>
-#include <WiFiMulti.h>
-#include <HTTPClient.h>
-#endif
+#include <WiFiClientSecure.h>
 
 class DeneyapTelegram
 {
 public:
   void begin(String token);
-  void kurulum(String token);
-
   bool send(String id, String content);
-  bool gonder(String id, String content);
-
 private:
   String token;
 };
